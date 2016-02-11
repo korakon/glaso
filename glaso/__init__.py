dc6a20deb7281d2e610d0e06381c23dbe9111874
Korakon <i@korakon.com>
2016-02-11 20:51:38 +0100

    first api expiriments.

--- glaso/__init__.py
+++ glaso/__init__.py
@@ -1,3 +1,29 @@
+from glaso import App, use, middleware
+from glaso.x import session
 
-def ayy():
-    print('lmao')
+app = App()
+
+@app.get('/')
+def home(x):
+    return 'Hello world!'
+
+@app.post('/users/')
+def users(x):
+    return 201, {'id': 1234}
+
+@app.put('/users/:username')
+def putusers(db, params, session, cookies):
+    user = db.User.get(params.username)
+    return 201, {'success': True}
+
+@middleware
+def cors(app):
+    def wrapper(ctx):
+        response = app(ctx)
+        response.headers['Access-Allow-Origin'] = '*'
+
+middlewares = use(error, cors, session)
+app = middlwares(dispatch(routes))
+
+if __name__ == '__main__':
+    run(app, port=3000)
